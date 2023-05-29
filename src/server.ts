0e78e92035c4e961afe212dce7433bdb309ff644
Ryan Paiva <ryantulio001@gmail.com>
2023-05-29 15:58:30 -0300

    add verificação de nomes duplicados

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,8 @@ const port = 3000;
 const app = express();
 const prisma = new PrismaClient();
 
+app.use(express.json());
+
 app.get("/movies", async (_, res) => {
     const movies = await prisma.movie.findMany({
         orderBy: {
@@ -16,6 +18,41 @@ app.get("/movies", async (_, res) => {
         }
     });
     res.json(movies);
+});
+
+app.post("/movies", async (req, res) => {
+
+    const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+
+    try {
+
+        //case insensitive - se a busca feita por john wick ou John wick ou JOHN WICK, o registro vai ser retornado na consulta
+
+        // case sensitive - se buscar por john wick e no banco estiver como John wick, não vai ser retornado na consulta
+        const movieWithSameTitle = await prisma.movie.findFirst({
+            where: {
+                title: { equals: title, mode: "insensitive" }
+            }
+        });
+
+        if (movieWithSameTitle) {
+            return res.status(409).send({ message: "Já existe um filme cadastrado com esse título" });
+        }
+
+        await prisma.movie.create({
+            data: {
+                title,
+                genre_id,
+                language_id,
+                oscar_count,
+                release_date: new Date(release_date),
+            }
+        });
+    } catch (error) {
+        return res.status(500).send({ message: "Falha ao cadastrar um filme" });
+    }
+
+    res.status(201).send();
 });
 
 app.listen(port, () => {
